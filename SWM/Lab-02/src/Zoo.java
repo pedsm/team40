f4891bfe63fbb1229ff5885128348ee8cd7b1295
rish1603 <rish1603@gmail.com>
2017-01-11 22:11:55 +0000

    finished part 1

--- SWM/Lab-02/src/Zoo.java
+++ SWM/Lab-02/src/Zoo.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 
 public class Zoo {
 
-	// constructors
+	// Constructors
 	public Zoo() {
 		this("Unknown", 30);
 	}
@@ -9,30 +10,42 @@ public class Zoo {
 	public Zoo(String location, int numEnclosures) {
 		this.location = location;
 		this.numEnclosures = numEnclosures;
-		numberOfZoos ++;
+		numberOfZoos++;
 		setZooID(numberOfZoos);
+		enclosures = new ArrayList<Enclosure>();
+		for (int i = 0; i < numEnclosures; i++) {
+			addEnclosure(new Enclosure());
+		}
 	}
 
-	//Field declarations
+	private void addEnclosure(Enclosure enclosure) {
+		this.enclosures.add(enclosure);
+	}
+
+	// Field declarations
 	private String location;
 	private int numEnclosures = 0;
 	private int zooID;
-	public static int numberOfZoos; //Static so all objects of the same type can access
+	public static int numberOfZoos; // Static so all objects of the same type
+									// can access
 
-	//Methods
+	// Initialise ArrayList
+	private ArrayList<Enclosure> enclosures;
+
+	// Methods
 	public void buildNewEnclosure() {
 		int numEnclosures;
 		numEnclosures = getNumEnclosures();
 		numEnclosures++;
 		setNumEnclosures(numEnclosures);
 	}
-	
+
 	public void printInfo() {
 		System.out.println("Location: " + getLocation());
 		System.out.println("Number of Enclosures " + getNumEnclosures() + "\n");
 	}
-	
-	//Accessor Methods
+
+	// Accessor Methods
 
 	public String getLocation() {
 		return location;
@@ -49,7 +62,7 @@ public class Zoo {
 	public void setNumEnclosures(int numEnclosures) {
 		this.numEnclosures = numEnclosures;
 	}
-	
+
 	public int getZooID() {
 		return zooID;
 	}
--- SWM/Lab-02/src/ZooApp.java
+++ SWM/Lab-02/src/ZooApp.java
@@ -1,4 +1,3 @@
-
 public class ZooApp {
 
 	public static void main(String[] args) {
