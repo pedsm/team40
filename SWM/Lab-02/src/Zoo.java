4e2d3332e8122f858292a4f2c2994dbc66dd085a
rish1603 <rish1603@gmail.com>
2017-01-11 19:51:04 +0000

    added zoos and shit

--- SWM/Lab-02/src/Zoo.java
+++ SWM/Lab-02/src/Zoo.java
@@ -9,11 +9,30 @@ public class Zoo {
 	public Zoo(String location, int numEnclosures) {
 		this.location = location;
 		this.numEnclosures = numEnclosures;
+		numberOfZoos ++;
+		setZooID(numberOfZoos);
 	}
 
-	// Prvate Fields
+	//Field declarations
 	private String location;
-	private int numEnclosures;
+	private int numEnclosures = 0;
+	private int zooID;
+	public static int numberOfZoos; //Static so all objects of the same type can access
+
+	//Methods
+	public void buildNewEnclosure() {
+		int numEnclosures;
+		numEnclosures = getNumEnclosures();
+		numEnclosures++;
+		setNumEnclosures(numEnclosures);
+	}
+	
+	public void printInfo() {
+		System.out.println("Location: " + getLocation());
+		System.out.println("Number of Enclosures " + getNumEnclosures() + "\n");
+	}
+	
+	//Accessor Methods
 
 	public String getLocation() {
 		return location;
@@ -30,5 +49,12 @@ public class Zoo {
 	public void setNumEnclosures(int numEnclosures) {
 		this.numEnclosures = numEnclosures;
 	}
+	
+	public int getZooID() {
+		return zooID;
+	}
 
+	public void setZooID(int zooID) {
+		this.zooID = zooID;
+	}
 }
